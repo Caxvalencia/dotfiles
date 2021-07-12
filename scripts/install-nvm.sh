964e0065f46a246a42a601dd82608a6feb9b438a
Caxvalencia <caxvalencia@gmail.com>
2021-07-11 23:04:06 -0500

    feat: update install nvm with node and updated into main file bash

--- scripts/install-nvm.sh
+++ scripts/install-nvm.sh
@@ -1,5 +1,13 @@
+NODE_VERSION=${NODE_VERSION:-14}
+
 if ! command_exists nvm; then
   echo_info "NVM not installed, trying to install"
-  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash 
+  bash_run "curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh"
   echo_success "NVM installed"
+fi
+
+if ! command_exists node; then
+  echo_info "nodejs $NODE_VERSION not installed, trying to install"
+  nvm use $NODE_VERSION
+  echo_success "nodejs $NODE_VERSION installed"
 fi
