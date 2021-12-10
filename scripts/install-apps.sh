6a2fd2662f07e87d8753da0d81096256b254e8c1
Caxvalencia <caxvalencia@gmail.com>
2021-12-09 22:33:42 -0500

    feat(apps): add gitignore file and add optional install for android setup

--- scripts/install-apps.sh
+++ scripts/install-apps.sh
@@ -6,4 +6,22 @@ INSTALL_APPS="$(eval echo "$INSTALL_APPS")"
 if [[ "$INSTALL_APPS" == "Y" ]] || [[ "$INSTALL_APPS" == "y" ]]; then
   echo_info "Installing applications (${cyan}Dependencies, Google Chrome, VScode...${normal}) via Homebrew"
   brew bundle --file "./Brewfile"
+
+  # CONFIGS
+  echo_info "Installing custom configuration for iterm2"
+  cp ./iterm2-profile.json "$HOME/Library/Application Support/iTerm2/DynamicProfiles"
+  echo_success "Configuration iterm2 files copied to $HOME/Library/Application Support/iTerm2/DynamicProfiles."
 fi
+
+# OPTIONALS APPLICATIONS
+echo_question "Do you like install android studio and sdk? [${purple}Y${normal}es/${purple}N${normal}o] default (N)" "OPTIONAL_APPS"
+
+OPTIONAL_APPS="${OPTIONAL_APPS:-"N"}"
+OPTIONAL_APPS="$(eval echo "$OPTIONAL_APPS")"
+
+if [[ "$OPTIONAL_APPS" == "Y" ]] || [[ "$OPTIONAL_APPS" == "y" ]]; then
+  brew install cask "android-studio"
+  brew install cask "android-sdk"
+fi
+
+echo_success "Applications installed!"
